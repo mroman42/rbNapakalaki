920d02ce05207edbd3e462a43dd885bd2790c3fb
M42 <mromang08@gmail.com>
2014-03-06 13:27:50 +0100

    Cambio del mensaje. Por mantener coherencia con Java.

--- monster.rb
+++ monster.rb
@@ -2,6 +2,7 @@
 #encoding: utf-8
 require_relative 'BadConsequence.rb'
 require_relative 'Prize.rb'
+
 module 'Napakalaki'
   class Monster
     def initialize(name, level, bad, prize)
@@ -12,12 +13,12 @@ module 'Napakalaki'
     end
 
     attr_reader :name
-		attr_reader :level
-		attr_reader :bad
-		attr_reader :prize
+    attr_reader :level
+    attr_reader :bad
+    attr_reader :prize
 
     def to_s()
-      puts "El monstruo #{@name} de nivel #{@level} te da #{victory.to_s()} si le ganas y si pierdes:\n#{defeat.to_s()}\n"
+      puts "#{@name} (lv. #{@level}) \nPrize: #{victory.to_s()} \n Bad: \n#{defeat.to_s()}\n"
     end
   
 	end
