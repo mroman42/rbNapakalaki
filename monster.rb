4dcefde6538f8b807e740f34fd4303935477d441
JCEntrena <jentrena@correo.ugr.es>
2014-03-06 13:17:06 +0100

    Modificados nombres.

--- monster.rb
+++ monster.rb
@@ -4,18 +4,22 @@ require_relative 'BadConsequence.rb'
 require_relative 'Prize.rb'
 module 'Napakalaki'
   class Monster
-    def initialize(nombre, nivel, buenrollo, malrollo)
-      @name = nombre
-      @level = nivel
-      @victory = buenrollo
-      @defeat = malrollo
+    def initialize(name, level, bad, prize)
+      @name = name 
+      @level = level
+      @bad = bad
+      @prize = prize
     end
 
-    attr_reader :name, :level, :victory, :defeat
+    attr_reader :name
+		attr_reader :level
+		attr_reader :bad
+		attr_reader :prize
 
     def to_s()
       puts "El monstruo #{@name} de nivel #{@level} te da #{victory.to_s()} si le ganas y si pierdes:\n#{defeat.to_s()}\n"
     end
-  end
+  
+	end
 end
 
