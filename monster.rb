42a481b6e07e4d7d6f28d39807d32b34c6ad6239
oxcar103 <oxcar103@outlook.com>
2014-03-06 11:57:57 +0100

    Añadidas monster.rb y pruebaNapakalaki.rb
    pruebaNapakalaki.rb está casi vacío

---
+++ monster.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+require_relative 'BadConsequence.rb'
+require_relative 'Prize.rb'
+module 'Napakalaki'
+    class Monster
+        def initialize(nombre, nivel, buenrollo, malrollo)
+            @name = nombre
+            @level = nivel
+            @victory = buenrollo
+            @defeat = malrollo
+        end
+
+        attr_reader :name, :level, :victory, :defeat
+
+        def to_s()
+            puts "El monstruo #{@name} de nivel #{@level} te da #{victory.to_s()} si le ganas y si pierdes:\n#{defeat.to_s()}\n"
+        end
+
+    end
+end
+
---
+++ pruebaNapakalaki.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+require_relative 'Monster.rb'
+module Napakalaki
+    class PruebaNapakalaki
+        if __FILE__ == $0
+            
+        end
+    end
+
+end
