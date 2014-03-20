77171daee67427eeb8d36b5d9c62bc1873c097ca
M42 <mromang08@gmail.com>
2014-03-20 12:02:23 +0100

    Treasure.

---
+++ treasure.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+require_relative 'treasureKind.rb'
+
+module Napakalaki
+  class Treasure
+    # Inicialización
+    def initialize (name, goldCoins, minBonus, maxBonus, type)
+      @name = name
+      @goldCoins = goldCoins
+      @minBonus = minBonus
+      @maxBonus = maxBonus
+      @type = type
+    end
+
+    # Getters
+    attr_reader :name
+    attr_reader :goldCoins
+    attr_reader :minBonus
+    attr_reader :maxBonus
+    attr_reader :type
+
+  end
+end
