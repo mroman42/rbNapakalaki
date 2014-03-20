fc73dd541237f52c1dcc5add9e26bf858fdab144
M42 <mromang08@gmail.com>
2014-03-20 11:50:19 +0100

    Añadido el jugador.

---
+++ player.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+
+module Napakalaki
+  
+  class Player
+    
+    def initialize (name, level, dead)
+      @name = name
+      @level = level
+      @dead = dead
+    end
+
+    
+  end
+
+end
