e7642fe47dfee62833a7746f7dd8d56e103f4492
M42 <mromang08@gmail.com>
2014-03-23 11:43:52 +0100

    Métodos de Napakalaki.

--- napakalaki.rb
+++ napakalaki.rb
@@ -7,9 +7,65 @@ module Game
   class CardDealer
     include Singleton
 
-    def self.main ()
-   
+    def initialize()
+      @player = nil
+      @currentPlayer = nil
+      @currentMonster = nil
     end
+
+    attr_reader :currentPlayer
+    attr_reader :currentMonster
+    attr_reader :player
+
+    def initPlayers(names)
+    end
+
+    def nextPlayer()
+    end
+
+    def combat()
+    end
+
+    def discardVisibleTreasure(treasure)
+    end
+
+    def discardHiddenTreasure(treasure)
+    end
+    
+    def makeTreasureVisible(treasure) 
+    end
+
+    def buyLevels(visible, hidden)
+    end
+
+    def initGame(players)
+    end
+
+    def getCurrentPlayer()
+    end
+
+    def getCurrentMonster()
+    end
+    
+    def canMakeTreasureVisible(treasure)
+    end
+
+    def getVisibleTreasures()
+    end
+
+    def getHiddenTreasures()
+    end
+
+    def nextTurn()
+    end
+
+    def nextTurnAllowed()
+    end
+
+    def endOfGame(result)
+    end
+
+  end
 
   if __FILE__ == $0
     PruebaNapakalaki.main()
