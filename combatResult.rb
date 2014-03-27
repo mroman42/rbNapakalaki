e57adf7c1da57251fa219cc74c8d5b3f2df79bf7
oxcar103 <oxcar103@outlook.com>
2014-03-27 12:38:00 +0100

    Clase combatResult

---
+++ combatResult.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+
+module Game
+	
+  # Resultados de un combate.
+  WINANDWINGAME = :winandwingame
+  WIN = :win
+  LOSE = :lose
+  LOSEANDESCAPE = :loseandescape
+  LOSEANDDIE = :loseanddie
+
+end
