bea31a9f7a1f0a56e251c2211e37fb8130637b13
M42 <mromang08@gmail.com>
2014-04-10 13:20:34 +0200

    Inicio de TextUI.

---
+++ textUI.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+require_relative 'napakalaki.rb'
+
+module TextUI
+
+  class TextUI
+    
+  end
+
+  if __FILE__ == $0
+    puts "Napakalaki\n"
+  end
+
+end
+
+
