d19732c6e11b79854dc4a73f5a1422222137c918
M42 <mromang08@gmail.com>
2014-04-15 16:58:51 +0200

    Inicio de la interfaz de texto. Lee el número de jugadores.

--- textUI.rb
+++ textUI.rb
@@ -2,15 +2,32 @@
 #encoding: utf-8
 require_relative 'napakalaki.rb'
 
-module TextUI
+module UserInterface
 
-  class TextUI
-    
-  end
+    class TextUI
+        include Singleton
 
-  if __FILE__ == $0
-    puts "Napakalaki\n"
-  end
+        def printHeader
+            puts "-"*30
+            puts "\tNapakalaki"
+            puts "-"*30
+        end
+
+        def readPlayers
+            puts "Introduzca el nombre de los jugadores:"
+            line = gets.chomp
+            players = line.split()
+            return players
+        end
+    end
+
+    if __FILE__ == $0
+        ui = TextUI.instance
+
+        ui.printHeader
+        players = ui.readPlayers
+        puts players
+    end
 
 end
 
