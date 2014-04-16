08d0fb22a80708fb7b47c8060bffa40270150ad1
M42 <mromang08@gmail.com>
2014-04-17 00:01:58 +0200

    Añadiendo @. Y de paso, cambiando las listas, de "nil" a listas vacías a las que se pueda hacer push.

--- cardDealer.rb
+++ cardDealer.rb
@@ -11,10 +11,10 @@ module Game
 
         # Constructor
         def initialize()
-            @unusedMonsters = nil
-            @usedMonsters = nil
+            @unusedMonsters = []
+            @usedMonsters = []
             @unusedTreasures = []
-            @usedTreasures = nil
+            @usedTreasures = []
         end
 
 
@@ -124,112 +124,112 @@ module Game
             # El profesor de PDOO.
             bad = BadConsequence.new_det_tr("Se pierden 20 niveles, todas las armaduras visibles y todos los tesoros de mano invisibles.", 20, [ARMOR], [ONEHAND, BOTHHANDS]*4)
             prize = Prize.new(1,2)
-            unusedMonsters.push(Monster.new("El profesor de PDOO", 25, bad, prize))
+            @unusedMonsters.push(Monster.new("El profesor de PDOO", 25, bad, prize))
 
             #Monstruos añadidos por orden de aparición en el guión. 
             # 3 Byakhees de bonanza.
             bad = BadConsequence.new_det_tr("Pierdes tu armadura visible y otra oculta.", 0, [ARMOR], [ARMOR])
             prize = Prize.new(2,1)
-            unusedMonsters.push(Monster.new("3 Byakhees de bonanza", 8, bad, prize))
+            @unusedMonsters.push(Monster.new("3 Byakhees de bonanza", 8, bad, prize))
 
             # Chibithulhu
             bad = BadConsequence.new_det_tr("Embobados con el lindo primigenio te descartas de tu casco visible.", 0, [HELMET], [])
             prize = Prize.new(1,1)
-            unusedMonsters.push(Monster.new("Chibithulhu", 2, bad, prize))
+            @unusedMonsters.push(Monster.new("Chibithulhu", 2, bad, prize))
 
             # El sopor de Dunwich
             bad = BadConsequence.new_det_tr("El primordial bostezo contagioso. Pierdes el calzado visible.", 0, [SHOE], [])
             prize = Prize.new(1,1)
-            unusedMonsters.push(Monster.new("El sopor de Dunwich", 2, bad, prize))
+            @unusedMonsters.push(Monster.new("El sopor de Dunwich", 2, bad, prize))
 
             # Ángeles de la noche ibicenca
             bad = BadConsequence.new_det_tr("Te atrapan para llevarte de fiesta y te dejan caer en mitad del vuelo. Descarta 1 mano visible y 1 mano oculta.", 0, [ONEHAND], [ONEHAND])
             prize = Prize.new(4,1)
-            unusedMonsters.push(Monster.new("Ángeles de la noche ibicenca", 14, bad, prize))
+            @unusedMonsters.push(Monster.new("Ángeles de la noche ibicenca", 14, bad, prize))
 
             # El gorrón en el umbral
             bad = BadConsequence.new_indet_tr("Pierdes todos tus tesoros visibles.", 0, 99, 0)
             prize = Prize.new(3,1)
-            unusedMonsters.push(Monster.new("El gorrón en el umbral", 10, bad, prize))
+            @unusedMonsters.push(Monster.new("El gorrón en el umbral", 10, bad, prize))
 
             # H.P. Munchcraft
             bad = BadConsequence.new_det_tr("Pierdes la armadura visible.", 0, [ARMOR], [])
             prize = Prize.new(2,1)
-            unusedMonsters.push(Monster.new("H.P. Munchcraft", 6, bad, prize))
+            @unusedMonsters.push(Monster.new("H.P. Munchcraft", 6, bad, prize))
 
             # Bichgooth
             bad = BadConsequence.new_det_tr("Sientes bichos bajo la ropa. Descarta la armadura visible.", 0, [ARMOR], []) 
             prize = Prize.new(1,1)
-            unusedMonsters.push(Monster.new("Bichgooth", 2, bad, prize))
+            @unusedMonsters.push(Monster.new("Bichgooth", 2, bad, prize))
 
             # El rey de rosa
             bad = BadConsequence.new_indet_tr("Pierdes 5 niveles y 3 tesoros visibles.", 5, 3, 0)
             prize = Prize.new(4,2)
-            unusedMonsters.push(Monster.new("El rey de rosa", 13, bad, prize))
+            @unusedMonsters.push(Monster.new("El rey de rosa", 13, bad, prize))
 
             # La que redacta en las sombras. 
             bad = BadConsequence.new_indet_tr("Toses los pulmones y pierdes 2 niveles.", 2, 0, 0)
             prize = Prize.new(1,1)
-            unusedMonsters.push(Monster.new("La que redacta en las sombras", 3, bad, prize))
+            @unusedMonsters.push(Monster.new("La que redacta en las sombras", 3, bad, prize))
 
             # Los hondos verdes
             bad = BadConsequence.new_death("Estos monstruos resultan bastante superficiales y te aburren mortalmente. Estás muerto.")
             prize = Prize.new(2,1)
-            unusedMonsters.push(Monster.new("Los hondos verdes", 7, bad, prize))
+            @unusedMonsters.push(Monster.new("Los hondos verdes", 7, bad, prize))
 
             # Semillas Cthulhu
             bad = BadConsequence.new_indet_tr("Pierdes 2 niveles y 2 tesoros ocultos.", 2, 0, 2)
             prize = Prize.new(2,1)
-            unusedMonsters.push(Monster.new("Semillas Cthulhu", 4, bad, prize))
+            @unusedMonsters.push(Monster.new("Semillas Cthulhu", 4, bad, prize))
 
             # Dameargo
             bad = BadConsequence.new_det_tr("Te intentas escapar. Pierdes una mano visible.", 0, [ONEHAND], [])
             prize = Prize.new(2,1)
-            unusedMonsters.push(Monster.new("Dameargo", 1, bad, prize))
+            @unusedMonsters.push(Monster.new("Dameargo", 1, bad, prize))
 
             # Pollipólipo volante
             bad = BadConsequence.new_indet_tr("Da mucho asquito. Pierdes 3 niveles.", 3, 0, 0)
             prize = Prize.new(1,1)
-            unusedMonsters.push(Monster.new("Pollipólipo volante", 3, bad, prize))
+            @unusedMonsters.push(Monster.new("Pollipólipo volante", 3, bad, prize))
 
             # Yskhtihyssg-Goth
             bad = BadConsequence.new_death("No le hace gracia que pronuncien mal su nombre. Estás muerto.")
             prize = Prize.new(3,1)
-            unusedMonsters.push(Monster.new("Yskhtihyssg-Goth", 12, bad, prize))
+            @unusedMonsters.push(Monster.new("Yskhtihyssg-Goth", 12, bad, prize))
 
             # Familia feliz.
             bad = BadConsequence.new_death("La familia te atrapa. Estás muerto.")
             prize = Prize.new(4,1)
-            unusedMonsters.push(Monster.new("Familia feliz", 1, bad, prize))
+            @unusedMonsters.push(Monster.new("Familia feliz", 1, bad, prize))
 
             # Roboggoth
             bad = BadConsequence.new_det_tr("La quinta directiva primaria te obliga a perder 2 niveles y un tesoro 2 manos visible.", 2, [BOTHHANDS], [])
             prize = Prize.new(2,1)
-            unusedMonsters.push(Monster.new("Roboggoth", 8, bad, prize))
+            @unusedMonsters.push(Monster.new("Roboggoth", 8, bad, prize))
 
             # El espía ciego.
             bad = BadConsequence.new_det_tr("Te asusta en la noche. Pierdes un casco visible.", 0, [HELMET], [])
             prize = Prize.new(1,1)
-            unusedMonsters.push(Monster.new("El espía ciego", 4, bad, prize))
+            @unusedMonsters.push(Monster.new("El espía ciego", 4, bad, prize))
 
             # El Lenguas
             bad = BadConsequence.new_indet_tr("Menudo susto te llevas. Pierdes 2 niveles y 5 tesoros visibles.", 2, 5, 0)
             prize = Prize.new(1,1)
-            unusedMonsters.push(Monster.new("El Lenguas", 20, bad, prize))
+            @unusedMonsters.push(Monster.new("El Lenguas", 20, bad, prize))
 
             # Bicéfalo 
             bad = BadConsequence.new_det_tr("Te faltan manos para tanta cabeza. Pierdes 3 niveles y tus tesoros visibles de las manos.", 3, [ONEHAND, ONEHAND, BOTHHANDS], [])
             prize = Prize.new(1,1)
-            unusedMonsters.push(Monster.new("Bicéfalo", 20, bad, prize))
+            @unusedMonsters.push(Monster.new("Bicéfalo", 20, bad, prize))
 
         end
         
         def shuffleTreasures()
-            unusedTreasures.shuffle
+            @unusedTreasures.shuffle
         end
 
         def shuffleMonsters()
-            unusedMonsters.shuffle
+            @unusedMonsters.shuffle
         end
 
 
@@ -238,24 +238,22 @@ module Game
 
         def nextTreasure
             #swap si unusedTreasures está vacío
-            unusedTreasures,usedTreasures = usedTreasures,unusedTreasures if (unusedTreasures.isempty?)
-
-            unusedTreasures.pop
+            @unusedTreasures,@usedTreasures = @usedTreasures,@unusedTreasures if (@unusedTreasures.isempty?)
+            @unusedTreasures.pop
         end
         
         def nextMonster
             #swap si unusedMonsters está vacío
-            unusedMonsters,usedMonsters = usedMonsters,unusedMonsters if (unusedMonsters.isempty?)
-
-            unusedMonsters.pop
+            @unusedMonsters,@usedMonsters = @usedMonsters,@unusedMonsters if (@unusedMonsters.isempty?)
+            @unusedMonsters.pop
         end
         
         def giveTreasureBack(treasure)
-            usedTreasures.push(treasure)
+            @usedTreasures.push treasure
         end
 
         def giveMonsterBack(monster)
-            usedMonsters.push(treasure)
+            @usedMonsters.push treasure
         end
 
         def initCards
