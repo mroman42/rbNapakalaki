d69875f8aaacf400eb19d2fe968fc7ecdad2ad90
M42 <mromang08@gmail.com>
2014-04-17 13:41:10 +0200

    Indentación a 4 espacios.

--- badConsequence.rb
+++ badConsequence.rb
@@ -4,121 +4,121 @@ require_relative 'treasureKind'
 
 module Game
 
-  # Perjuicio al producirse la derrota.
-  class BadConsequence
-    
-    # Inicializador
-    def initialize (text, levels, nVisible, nHidden, visible, hidden, death)
-      @text = text
-      @levels = levels
-      @nVisibleTreasures = nVisible
-      @nHiddenTreasures = nHidden
-      @specificVisibleTreasures = visible
-      @specificHiddenTreasures = hidden
-      @death = death
+    # Perjuicio al producirse la derrota.
+    class BadConsequence
+        
+        # Inicializador
+        def initialize (text, levels, nVisible, nHidden, visible, hidden, death)
+            @text = text
+            @levels = levels
+            @nVisibleTreasures = nVisible
+            @nHiddenTreasures = nHidden
+            @specificVisibleTreasures = visible
+            @specificHiddenTreasures = hidden
+            @death = death
+        end
+
+
+
+        # Constructores
+        def self.new_det_tr(text, levels, visible, hidden)
+            newObj=allocate
+            newObj.send(:initialize, text, levels, visible.size, hidden.size, visible, hidden, false)
+            newObj
+        end
+
+        def self.new_indet_tr(text, levels, nVisible, nHidden)
+            newObj=allocate
+            newObj.send(:initialize, text, levels, nVisible, nHidden, [], [], false)
+            newObj
+        end
+
+        def self.new_death(text)
+            newObj=allocate
+            newObj.send(:initialize, text, 0, 0, 0, [], [], true)
+            newObj
+        end
+
+
+
+        # Métodos públicos
+        # HAY QUE COMENTAR LOS MÉTODOS (Los importantes al menos, los getters no hará falta)
+        
+        # Las dos últimas condiciones pueden traer problemas: Si NHTreasures es 7, como solo se pueden tener 4, al quitarlos quedarán 3. 
+        def isEmpty
+            (death == false && levels == 0 && nVisibleTreasures == 0 && nHiddenTreasures == 0)
+        end 
+
+        def kills
+            @death
+        end
+
+        def getLevels
+            @levels
+        end
+
+        def getNVisibleTreasures
+            @nVisibleTreasures
+        end
+
+        def getNHiddenTreasures
+            @nHiddenTreasures
+        end
+
+        def getSpecificVisibleTreasures
+            @specificVisibleTreasures
+        end
+
+        def getSpecificHiddenTreasures
+            @specificHiddenTreasures
+        end
+
+        def substractVisibleTreasure(treasure)
+            if(specificVisibleTreasures.include? treasure || (specificVisibleTreasures.isempty? && nVisibleTreasures != 0))
+                specificVisibleTreasures - treasure
+                nVisibleTreasures -= 1
+            end
+        end
+
+        def substractHiddenTreasure(treasure)
+            if(specificHiddenTreasures.include? treasure || (specificHiddenTreasures.isempty? && nHiddenTreasures != 0))
+                specificHiddenTreasures - treasure
+                nHiddenTreasures -= 1 
+            end
+        end
+
+        def adjustToFitTreasureLists(visible, hidden)        
+            if(death)
+                return BadConsequence.new_death(text, death)
+
+            elsif (specificVisibleTreasures.empty? && specificHiddenTreasures.empty?)
+                nVTreasures = [visibles.size, nVisibleTreasures].min
+                nHTreasures = [hidden.size, nHiddenTreasures].min
+
+                return BadConsequence.new_indet_tr(text, levels, nVTreasures, nHTreasures)
+
+            else
+                listVisibleTreasures = visible & specificVisibleTreasures
+                listHiddenTreasures = hidden & specificHiddenTreasures
+
+                return BadConsequence.new_det_tr(text, levels, listVisibleTreasures, listHiddenTreasures);
+            end
+        end 
+
+
+        # Métodos auxiliares
+        def to_s()
+            "#{@text} \n\t" + (death ? "DEATH." : 
+                               "#{@levels} levels, #{@nHidden} hidden treasures, #{@nVisible} visible treasures." 
+                               ) + "\n"
+        end
+
     end
 
 
 
-    # Constructores
-    def self.new_det_tr(text, levels, visible, hidden)
-      newObj=allocate
-      newObj.send(:initialize, text, levels, visible.size, hidden.size, visible, hidden, false)
-      newObj
-    end
-
-    def self.new_indet_tr(text, levels, nVisible, nHidden)
-      newObj=allocate
-      newObj.send(:initialize, text, levels, nVisible, nHidden, [], [], false)
-      newObj
-    end
-
-    def self.new_death(text)
-      newObj=allocate
-      newObj.send(:initialize, text, 0, 0, 0, [], [], true)
-      newObj
-    end
-
-
-
-    # Métodos públicos
-    # HAY QUE COMENTAR LOS MÉTODOS (Los importantes al menos, los getters no hará falta)
-    
-    # Las dos últimas condiciones pueden traer problemas: Si NHTreasures es 7, como solo se pueden tener 4, al quitarlos quedarán 3. 
-    def isEmpty
-      (death == false && levels == 0 && nVisibleTreasures == 0 && nHiddenTreasures == 0)
-    end 
-
-    def kills
-      @death
-    end
-
-    def getLevels
-      @levels
-    end
-
-    def getNVisibleTreasures
-      @nVisibleTreasures
-    end
-
-    def getNHiddenTreasures
-      @nHiddenTreasures
-    end
-
-    def getSpecificVisibleTreasures
-      @specificVisibleTreasures
-    end
-
-    def getSpecificHiddenTreasures
-      @specificHiddenTreasures
-    end
-
-    def substractVisibleTreasure(treasure)
-      if(specificVisibleTreasures.include? treasure || (specificVisibleTreasures.isempty? && nVisibleTreasures != 0))
-        specificVisibleTreasures - treasure
-        nVisibleTreasures -= 1
-      end
-    end
-
-    def substractHiddenTreasure(treasure)
-      if(specificHiddenTreasures.include? treasure || (specificHiddenTreasures.isempty? && nHiddenTreasures != 0))
-        specificHiddenTreasures - treasure
-        nHiddenTreasures -= 1 
-      end
-    end
-
-    def adjustToFitTreasureLists(visible, hidden)        
-      if(death)
-        return BadConsequence.new_death(text, death)
-
-      elsif (specificVisibleTreasures.empty? && specificHiddenTreasures.empty?)
-        nVTreasures = [visibles.size, nVisibleTreasures].min
-        nHTreasures = [hidden.size, nHiddenTreasures].min
-
-        return BadConsequence.new_indet_tr(text, levels, nVTreasures, nHTreasures)
-
-      else
-        listVisibleTreasures = visible & specificVisibleTreasures
-        listHiddenTreasures = hidden & specificHiddenTreasures
-
-        return BadConsequence.new_det_tr(text, levels, listVisibleTreasures, listHiddenTreasures);
-      end
-    end 
-
-
-    # Métodos auxiliares
-    def to_s()
-      "#{@text} \n\t" + (death ? "DEATH." : 
-                         "#{@levels} levels, #{@nHidden} hidden treasures, #{@nVisible} visible treasures." 
-                         ) + "\n"
-    end
-
-  end
-
-
-
-  # Deshabilitando el constructor por defecto
-  BadConsequence.instance_eval { undef :new }
+    # Deshabilitando el constructor por defecto
+    BadConsequence.instance_eval { undef :new }
 
 end
 
--- player.rb
+++ player.rb
@@ -6,156 +6,151 @@ require_relative 'treasure.rb'
 require_relative 'badConsequence.rb'
 
 module Game
-  
-  class Player
     
-    # Inicializador
-    def initialize(name)
-      @dead = true
-      @name = name
-      @level = 1
-      @pendingBadConsequence = nil
-      @hiddenTreasures = []
-      @visibleTreasures = []
+    class Player
+        
+        # Inicializador
+        def initialize(name)
+            @dead = true
+            @name = name
+            @level = 1
+            @pendingBadConsequence = nil
+            @hiddenTreasures = []
+            @visibleTreasures = []
 
-      bringToLife
-    end
-
-
-
-    # Métodos privados
-    private
-
-    def bringToLife()
-      dead = false
-      level = 1
-    end
-
-    def incrementLevels(increment)
-      level += increment
-    end
-
-    def decrementLevels(decrement)
-      level -= decrement
-      if (level < 1) 
-        level = 1
-      end 
-    end
-
-    def setPendingBadConsequence(badConsequence)
-      @pendingBadConsequence = badConsequence
-    end
-
-    def die()
-      hiddentreasure.each {|treasure| CardDealer.getInstance.giveTreasureBack(treasure)}
-      hiddentreasures.clean()
-      visibletreasure.each {|treasure| CardDealer.getInstance.giveTreasureBack(treasure)}
-      visibletreasure.clean()
-      dead = true
-    end
-
-    def discardNecklaceIfVisible
-      visibleTreasures-[NECKLACE]
-    end
-
-    def dieIfNoTreasures
-      if (visibleTreasures + hiddenTreasures).empty?
-        dead = true
-      end
-    end
-
-    def computeGoldCoinsValue(treasure)
-      value = 0
-      treasure.each {|t| value += getGoldCoins(t)}
-      value 
-    end
-
-    def canIBuyLevels(levels)
-      (levels + level) < 10
-    end
-
-
-
-    # Métodos públicos
-    public
-    
-    def applyPrize(prize)
-      incrementLevels(prize.getLevels)
-      nPrize = prize.getTreasures
-
-      min(nPrize, 4-hiddenTreasures.size).times do
-        hiddenTreasures.add(CardDealer.getInstance.nextTreasure)
-      end
-    end
-
-    def combat(monster)
-      total_level = getCombatLevel()
-    end
-
-    def applyBadConsequence(bad)
-      decrementLevels(bad.getLevels)
-      setPendingBadConsequence(bad.adjustToFiTreasureLists(visibleTreasures, hiddenTreasures))
-    end
-
-    def makeTreasureVisible(treasure)
-    end
-    
-    def canMakeTreasureVisible(treasure)
-    end
-
-    def discardVisibleTreasure(treasure)
-      visibleTreasures - treasure.getType()
-    end
-
-    def discardHiddenTreasure(treasure)
-      hiddenTreasures - treasure.getType()
-    end
-
-    def buyLevels(visible, hidden)
-    end
-
-    def getCombatLevel
-      combat_level = level;
-
-      visibleTreasures.each {|treasure|
-        if(visibleTreasures.include?(NECKLACE))
-          combat_level += treasure.getMaxBonus();
-        else
-          combat_level += treasure.getMinBonus();
+            bringToLife
         end
-      }
 
-      combat_level
+
+
+        # Métodos privados
+        private
+
+        def bringToLife()
+            dead = false
+            level = 1
+        end
+
+        def incrementLevels(increment)
+            level += increment
+        end
+
+        def decrementLevels(decrement)
+            level -= decrement
+            if (level < 1) 
+                level = 1
+            end 
+        end
+
+        def setPendingBadConsequence(badConsequence)
+            @pendingBadConsequence = badConsequence
+        end
+
+        def die()
+            hiddentreasure.each {|treasure| CardDealer.getInstance.giveTreasureBack(treasure)}
+            hiddentreasures.clean()
+            visibletreasure.each {|treasure| CardDealer.getInstance.giveTreasureBack(treasure)}
+            visibletreasure.clean()
+            dead = true
+        end
+
+        def discardNecklaceIfVisible
+            visibleTreasures-[NECKLACE]
+        end
+
+        def dieIfNoTreasures
+            if (visibleTreasures + hiddenTreasures).empty?
+                dead = true
+            end
+        end
+
+        def computeGoldCoinsValue(treasure)
+            value = 0
+            treasure.each {|t| value += getGoldCoins(t)}
+            value 
+        end
+
+        def canIBuyLevels(levels)
+            (levels + level) < 10
+        end
+
+
+
+        # Métodos públicos
+        public
+        
+        def applyPrize(prize)
+            incrementLevels(prize.getLevels)
+            nPrize = prize.getTreasures
+
+            min(nPrize, 4-hiddenTreasures.size).times do
+                hiddenTreasures.add(CardDealer.getInstance.nextTreasure)
+            end
+        end
+
+        def combat(monster)
+            total_level = getCombatLevel()
+        end
+
+        def applyBadConsequence(bad)
+            decrementLevels(bad.getLevels)
+            setPendingBadConsequence(bad.adjustToFiTreasureLists(visibleTreasures, hiddenTreasures))
+        end
+
+        def makeTreasureVisible(treasure)
+        end
+        
+        def canMakeTreasureVisible(treasure)
+        end
+
+        def discardVisibleTreasure(treasure)
+            visibleTreasures - treasure.getType()
+        end
+
+        def discardHiddenTreasure(treasure)
+            hiddenTreasures - treasure.getType()
+        end
+
+        def buyLevels(visible, hidden)
+        end
+
+        def getCombatLevel
+            combat_level = level;
+
+            visibleTreasures.each {|treasure|
+                if(visibleTreasures.include?(NECKLACE))
+                    combat_level += treasure.getMaxBonus();
+                else
+                    combat_level += treasure.getMinBonus();
+                end
+            }
+
+            combat_level
+        end
+
+        def validState
+            @pendingBadConsequence.isEmpty
+        end
+
+        def initTreasures
+        end
+
+        def isDead
+            @dead
+        end 
+
+        def hasVisibleTreasures
+            visibleTreasures.empty?
+        end
+
+        def getVisibleTreasures
+            @visibleTreasures
+        end
+
+        def getHiddenTreasures
+            @hiddenTreasures
+        end
+
     end
-
-    def validState
-        # Según el guión hay que escribir esto:
-        # @pendingBadConsequence.isEmpty? 
-        # Pero esto supone que pendingBadConsequence sea una lista, ¿lo es?
-
-		# No, pero la clase BadConsequence tiene el método isEmpty implementado. Supongo que se le ha colado la '?'
-        @pendingBadConsequence.isEmpty
-    end
-
-    def initTreasures
-    end
-
-    def isDead
-      @dead
-    end 
-
-    def hasVisibleTreasures
-      visibleTreasures.empty?
-    end
-
-    def getVisibleTreasures
-      @visibleTreasures
-    end
-
-    def getHiddenTreasures
-      @hiddenTreasures
-    end
-
-  end
 
 end
