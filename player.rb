54aa31002df1f56637753a10f134268047381adf
M42 <mromang08@gmail.com>
2014-05-06 16:12:23 +0200

    Simplificando el canMakeTreasureVisible. Usando el any? como método propio de Ruby. Es buena idea buscar métodos que simplifiquen el código que se traduce desde Java.

--- player.rb
+++ player.rb
@@ -156,41 +156,17 @@ module Game
 
             # 2. Si no es de mano(una o dos), se puede hacer visible si no hay otro del mismo tipo.
             elsif (type != ONEHAND and type != BOTHHANDS)
-                # Hay que hacer esto porque visibleTreasures tiene tesoros, no TreasureKind (no es posible ver si lo contiene directamente)
-                has_one = false
-                @visibleTreasures.each do |treasure|
-                    if treasure.getType == type
-                        has_one = true
-                    end
-                end
-                return (!has_one)
+                return @visibleTreasures.any? {|trs| trs.getType == type}
 
             # 3. Si es de una mano, puede hacerse visible si no hay ya dos de una mano visibles o uno de dos manos. 
             elsif (type == ONEHAND)
-                has_bothhands = false
-                onehands = 0
-                @visibleTreasures.each do |treasure|
-                    if treasure.getType == ONEHAND
-                        onehands = onehands + 1
-                    elsif treasure.getType == BOTHHANDS
-                        has_bothhands = true
-                    end
-                end
-                return (!has_bothhands and onehands < 2)
+                has_two_onehand = @visibleTreasures.select{|trs| trs.getType == ONEHAND}.size < 2
+                has_bothhand = @visibleTreasures.any? {|trs| trs.getType == BOTHHANDS}
+                return has_bothhand and has_two_onehand
             
             # 4. Si es de dos manos, puede hacerse visible si no hay ningún tesoro de mano equipado. 
             else 
-                has_bothhands = false
-                has_onehands = false
-                # Es posible que esta bucle pueda pararse al encontrar un true, pero no sé cómo. 
-                @visibleTreasures.each do |treasure|
-                    if treasure.getType == ONEHAND
-                        has_onehands = true
-                    elsif treasure.getType == BOTHHANDS
-                        has_bothhands = true
-                    end
-                end
-                return (!has_bothhands and !has_onehands)
+                return @visibleTreasures.any? {|trs| trs.getType == ONEHAND or trs.getType == BOTHHANDS}
             end 
         end
         
